84b3b533ff3ecd64113fda8de335bb1cbbb44c75
XscapeCn <648399901@qq.com>
2021-03-11 11:07:36 +0800

    add something interesting

--- src/TransferBed.java
+++ src/TransferBed.java
@@ -1,8 +1,10 @@
+import java.awt.desktop.SystemEventListener;
 import java.io.*;
 import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class TransferBed {
@@ -11,11 +13,13 @@ public class TransferBed {
 
     public static void main(String[] args) {
 //        String file = "C:/Users/Administrator/Desktop/A_E1_conservation_forSamtools.bed";
-        TransferBed tf = new TransferBed(args[0]);
+        for (int i = 0; i < args.length; i++) {
+            TransferBed tf = new TransferBed(args[i]);
+        }
     }
 
     public TransferBed(String filename){
-        if (filename.startsWith("A")){
+        if (filename.contains("A")){
             this.table.put("0", 480980714);
             this.table.put("chr1", 471304005);
             this.table.put("chr2", 462376173);
@@ -32,7 +36,7 @@ public class TransferBed {
             this.table2.put("chr5", 25);
             this.table2.put("chr6", 31);
             this.table2.put("chr7", 37);
-        } else if (filename.startsWith("B")){
+        } else if (filename.contains("B")){
             this.table.put("0", 480980714);
             this.table.put("chr1", 438720154);
             this.table.put("chr2", 453218924);
@@ -41,6 +45,25 @@ public class TransferBed {
             this.table.put("chr5", 451372872);
             this.table.put("chr6", 452077197);
             this.table.put("chr7", 453822637);
+//            this.table = (HashMap<String, Integer>) Map.of(
+//                    "0", 480980714,
+//                    "chr1", 438720154,
+//                    "chr2", 453218924,
+//                    "chr3", 448155269,
+//                    "chr4", 451014251,
+//                    "chr5", 451372872,
+//                    "chr6", 452077197,
+//                    "chr7", 453822637);
+//
+//            this.table2 = (HashMap<String, Integer>) Map.of(
+////                    "0", 480980714,
+//                    "chr1", 3,
+//                    "chr2", 9,
+//                    "chr3", 15,
+//                    "chr4", 21,
+//                    "chr5", 27,
+//                    "chr6", 33,
+//                    "chr7", 39);
 
             this.table2.put("chr1", 3);
             this.table2.put("chr2", 9);
@@ -49,7 +72,7 @@ public class TransferBed {
             this.table2.put("chr5", 27);
             this.table2.put("chr6", 33);
             this.table2.put("chr7", 39);
-        } else if (filename.startsWith("D")){
+        } else if (filename.contains("D")){
             this.table.put("0", 480980714);
             this.table.put("chr1", 452179604);
             this.table.put("chr2", 462216879);
@@ -68,13 +91,16 @@ public class TransferBed {
             this.table2.put("chr7", 41);
         }
 
-        readFile(filename);
+//        readFile(filename);
+        splitBed(filename);
+
+
     }
 
     public List<String[]> readFile(String filename){
 
-        Thread aaaaaa = new Thread();
-        aaaaaa.start();
+//        Thread aaaaaa = new Thread();
+//        aaaaaa.start();
         List<String[]> res = new ArrayList<>();
         String file = filename.split("\\.")[0] + "_transfer.bed";
         try {
@@ -95,10 +121,10 @@ public class TransferBed {
                     bw.write(re[2] + "\n");
                 } else {
                     bw.write(this.table2.get(re[0]) + 1 + "\t");
-                    int  aa = Integer.valueOf(re[1]) - this.table.get(re[0]);
+                    int  aa = Integer.parseInt(re[1]) - this.table.get(re[0]);
                     bw.write(aa + "\t");
 //                    bw.write("\t");
-                    int  bb = Integer.valueOf(re[2]) - this.table.get(re[0]);
+                    int  bb = Integer.parseInt(re[2]) - this.table.get(re[0]);
                     bw.write(bb + "\n");
                 }
             }
@@ -109,5 +135,49 @@ public class TransferBed {
         }
         return res;
     }
+
+    public void splitBed(String filename){
+//        List<String[]> res = new ArrayList<>();
+        int index = Integer.parseInt(String.valueOf(filename.charAt(0)));
+        String key = "chr" + index;
+        String file1 = filename.split("\\.")[0] + ".1.transfer.bed";
+        String file2 = filename.split("\\.")[0] + ".2.transfer.bed";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            BufferedWriter bw1 = new BufferedWriter(new FileWriter(file1));
+            BufferedWriter bw2 = new BufferedWriter(new FileWriter(file2));
+
+            String str;
+            while ((str = br.readLine()) != null){
+                String[] temp = str.split("\t");
+                if (Integer.parseInt(temp[2]) < table.get(key)){
+                    bw1.write(table2.get(key) + "\t");
+//                    bw1.write("\t");
+                    for (int i = 1; i < temp.length; i++) {
+                        bw1.write(temp[i] + "\t");
+                    }
+                    bw1.write("\n");
+                }else{
+                    bw2.write(table2.get(key)+1 + "\t");
+//                    bw2.write("\t");
+                    bw2.write(Integer.parseInt(temp[1]) - table.get(key) + "\t");
+//                    bw2.write("\t");
+                    bw2.write(Integer.parseInt(temp[2]) - table.get(key) + "\t");
+//                    bw2.write("\t");
+                    bw2.write(temp[3]);bw2.write("\t");
+                    bw2.write(temp[4]);bw2.write("\t");
+                    bw2.write("\n");
+                }
+            }
+            bw1.close();
+            bw2.close();
+            br.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
 
 }
--- src/burnAfterRead.java
+++ src/burnAfterRead.java
@@ -5,11 +5,42 @@ import java.util.List;
 public class burnAfterRead {
     public static void main(String[] args) {
 
+        MyInterFace<String> mf = (String s) -> System.out.println(s);
+        //lambda 用于函数式接口的实现。接口的实现需要一个函数的传入，类似method4。 此时lambda相当于一个函数（编程意义上的函数，参数，方法体，返回值）。
+        mf.test("This is method1, lambda");
 
+        MF<String> mf2 = new MF<>();
+        mf2.test("This is method2, class implements interface");
 
+        MyInterFace<String> mf3 = new MyInterFace<String>() {
+            @Override
+            public void test(String avr) {
+                System.out.println(avr);
+            }
+        };
+        mf3.test("This is method3, Inner override");
 
-
+        MyInterFace<String> mff = burnAfterRead::charByChar;
+        mff.test("This is method4, method reference");
 
     }
 
+    public static void charByChar(String var){
+        for (int i = 0; i < var.length(); i++) {
+            System.out.print(var.charAt(i));
+            System.out.print("-");
+        }
+    }
 }
+
+interface MyInterFace<T>{
+    void test(T avr);
+}
+
+class MF<T> implements MyInterFace<T>{
+    @Override
+    public void test(T var) {
+        System.out.println(var);
+    }
+}
+
